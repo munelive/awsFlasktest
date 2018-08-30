f2bbec43f067f8c63beb80088bd19716aa78c35c
Mune <mune@Munes-MacBook-Pro.local>
2018-08-30 00:32:14 -0500

    2nd commit

--- awsFlaskTestScript.py
+++ awsFlaskTestScript.py
@@ -1,1 +1,30 @@
-print('To set Flasks')
+from flask import Flask
+
+
+#-------------------------------------------------------
+#Setup flask
+app = Flask(__name__)
+PORT = 5000
+DEBUG = True
+
+@app.errorhandler(404)
+def not_found(error):
+    return "Not Found."
+
+
+#-------------------------------------------------------
+#init
+@app.route('/', methods=['POST','GET'])
+def init():
+    print('With flask')
+    return 'App connections with: 1. /webhookasana: To connect asana to todoist /// 2. /webhooktodoist: To connect Todoist to asana'
+
+
+
+
+
+
+
+#-------------------------------------------------------
+if __name__ == '__main__':
+    app.run(port = PORT, debug = DEBUG)
