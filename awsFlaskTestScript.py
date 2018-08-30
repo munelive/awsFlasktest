1b47043e8140ea60ae211b2d4cca05d73459510b
Mune <mune@Munes-MacBook-Pro.local>
2018-08-30 10:50:27 -0500

    3rd

--- awsFlaskTestScript.py
+++ awsFlaskTestScript.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 #-------------------------------------------------------
 #Setup flask
 app = Flask(__name__)
@@ -16,7 +15,7 @@ def not_found(error):
 #init
 @app.route('/', methods=['POST','GET'])
 def init():
-    print('With flask')
+    print('With flask2')
     return 'App connections with: 1. /webhookasana: To connect asana to todoist /// 2. /webhooktodoist: To connect Todoist to asana'
 
 
